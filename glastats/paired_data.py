bc81fa3447f481b0ef8b50836d91e315f7463a49
Matus Ferech <matus.ferech@telekom.com>
2018-01-26 19:39:30 +0000

    Add tests for paired_data

--- glastats/paired_data.py
+++ glastats/paired_data.py
@@ -7,22 +7,22 @@ from statistics import mean, variance as sample_variance
 
 def fisher(num, precision=4):
     num = Decimal(num)
-    return round(log((1 + num) / (1 - num)), precision)
+    return round(0.5 * log((1 + num) / (1 - num)), precision)
 
 
 def inverse_fisher(num, precision=4):
     num = Decimal(num)
-    return round((exp(2 * num) - 1) / (exp(2 * num) + 1), 4)
+    return round((exp(2 * num) - 1) / (exp(2 * num) + 1), precision)
 
 
-def sample_correlation(sxy, sxx, syy):
-    sxy, sxx, syy = Decimal(sxy), Decimal(sxx), Decimal(syy)
-    return sxy / sqrt(sxx * sxy)
+def sample_correlation(sxy, sxx, syy, precision=4):
+    return round(sxy / sqrt(sxx * syy), precision)
 
 
-def sample_covariance(x, y):
+def sample_covariance(x, y, precision=4):
     x_bar =  mean(x)
     y_bar =  mean(y)
     n = len(x)
 
-    return sum(x_i * y_i - n * x_bar * y_bar for x_i, y_i in zip(x, y))
+    cov = sum((x_i - x_bar) * (y_i - y_bar) for x_i, y_i in zip(x, y)) / (n - 1)
+    return round(cov, precision)
--- tests/test_paired_data.py
+++ tests/test_paired_data.py
@@ -3,4 +3,24 @@
 import unittest
 import env
 
-from glastats.paired_data import fisher
+from glastats.paired_data import (fisher, inverse_fisher, sample_covariance,
+                                  sample_correlation)
+
+class TestPairedData(unittest.TestCase):
+    def test_fisher(self):
+        self.assertEqual(fisher(0.296), 0.3051)
+
+    def test_inverse_fisher(self):
+        self.assertEqual(inverse_fisher(0.3051), 0.296)
+
+    def test_sample_correlation(self):
+        self.assertEqual(sample_correlation(5325, 177, 174757), 0.9574)
+
+    def test_sample_covariance(self):
+        x = [2.1, 2.5, 4.0, 3.6]
+        y = [8, 12, 14, 10]
+
+        self.assertEqual(sample_covariance(x, y), 1.5333)
+
+if __name__ == '__main__':
+    unittest.main()
