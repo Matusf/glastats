4e8b000576cb8f8420228eefbae75ff0ef68dfb7
Matus Ferech <matus.ferech@telekom.com>
2018-01-27 18:04:06 +0000

    Update LinearRegression

--- glastats/regression.py
+++ glastats/regression.py
@@ -1,26 +1,32 @@
 #!/usr/bin/env python3
 
 from statistics import mean
-from .paired_data import sample_covariance
+from .paired_data import sample_covariance, sample_variance
 
 
 class LinearRegression(object):
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self._alpha = alpha
-        self._beta = beta
         self._x_bar = mean(x)
         self._y_bar = mean(y)
+        self._beta = self.beta
+        self._alpha = self.alpha
+
+    def __repr__(self):
+        return 'LinearRegression(alpha={0}, beta={1})'.format(
+                round(self.alpha, 4), round(self.beta, 4))
 
     @property
-    def alpha(self, x, y):
-        return self._y_bar - _beta * self._x_bar
+    def alpha(self):
+        return self._y_bar - self.beta * self._x_bar
 
     @property
-    def beta(self, x, y):
-        return sample_covariance(x, y) / sample_variance(x)
+    def beta(self):
+        return sample_covariance(self.x, self.y) / sample_variance(self.x)
 
-    def fit(self, x, y):
-        beta = sample_covariance(x, y) / sample_variance(x)
-        alpha = self._y_bar - _beta * self._x_bar
+    def predict(self, x, precision=4):
+        if not hasattr(x, '__contains__'):
+            x = [x]
+
+        return [round(self.alpha + self.beta * x_i, precision) for x_i in x]
